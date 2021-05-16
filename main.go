39d942272004ed23aea764669afc9813592abf04
Ryan <error.nointernet@gmail.com>
2021-05-16 16:49:58 +0800

    Fixed Windows bug

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	if runtime.GOOS == "linux" {
 		databasePath = "/home/" + userObject.Username + "/.OkDatabase"
 	} else if runtime.GOOS == "windows" {
-		databasePath = "C:\\Users\\" + userObject.Username + "\\Documents\\OkDatabase"
+		databasePath = userObject.HomeDir + "\\OkDatabase"
 	}
 
 	rand.Seed(time.Now().UnixNano())

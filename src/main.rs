c804659458083a26068ce3f796ebdc40fbf017e5
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-04 18:47:13 +0800

    Add load_database

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 mod database;
 use database::Database;
 
-fn main() {
+fn main() {}
+
+fn load_database() -> Database {
     let mut database_path = String::from(".ok");
     if cfg!(windows) {
         database_path = format!("C:\\Users\\{}\\AppData\\Roaming\\ok", whoami::username())
     } else if cfg!(unix) {
         database_path = format!("/home/{}/.config/ok", whoami::username())
     }
-    let db = match Database::open(database_path) {
-        Ok(db) => db,
-        Err(error) => {
-            println!("Unable to open database: {}", error);
-            return;
-        }
-    };
+    Database::open(database_path).unwrap()
 }

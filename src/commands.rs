737c1e5fe2c6eb951fc1223d7e8219a7fbef8b8f
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-04 21:39:07 +0800

    Add database parameters

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,13 @@
-pub fn statistics_command() {
+use crate::database::Database;
+
+pub fn statistics_command(db: &Database) {
     println!("not implemented");
 }
-pub fn leaderboard_join_command() {
+
+pub fn leaderboard_join_command(db: &Database) {
     println!("not implemented");
 }
-pub fn leaderboard_leave_command() {
+
+pub fn leaderboard_leave_command(db: &Database) {
     println!("not implemented");
 }

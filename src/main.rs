1b6601ece4303b61f4d684dbab1f561d0ab737eb
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-04 21:39:14 +0800

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 mod commands;
 mod database;
 
-use std::collections::HashMap;
-
 use clap::Command;
+use colored::Colorize;
 use database::Database;
+use rand::Rng;
 
 fn main() {
-    let mut command = Command::new("ok")
-        .author("ErrorNoInternet")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("statistics")
-                .about("See your OK statistics")
-                .alias("stats")
-                .alias("status"),
-        );
+    let mut command = Command::new("ok").author("ErrorNoInternet").subcommand(
+        Command::new("statistics")
+            .about("See your OK statistics")
+            .alias("stats")
+            .alias("status"),
+    );
     if cfg!(feature = "online") {
         command = command
             .clone()
@@ -30,16 +26,49 @@ fn main() {
 
     match command.get_matches().subcommand() {
         Some(("statistics", _)) => {
-            commands::statistics_command();
+            commands::statistics_command(&load_database());
         }
         Some(("join", _)) => {
-            commands::leaderboard_join_command();
+            commands::leaderboard_join_command(&load_database());
         }
         Some(("leave", _)) => {
-            commands::leaderboard_leave_command();
+            commands::leaderboard_leave_command(&load_database());
         }
-        _ => unreachable!(),
+        _ => ok(&load_database()),
     }
+}
+
+fn ok(db: &Database) {
+    let counter: u128 = match db.get(String::from("counter")) {
+        Ok(counter) => match counter.parse() {
+            Ok(counter) => counter,
+            Err(_) => 0,
+        },
+        Err(_) => {
+            println!("Welcome, my friend, the the land of OKs. Here's your first OK:");
+            0
+        }
+    };
+    match db.set(String::from("counter"), (counter + 1).to_string()) {
+        Ok(_) => (),
+        Err(error) => println!("Uh oh! There was an error: {}", error),
+    };
+    print_rainbow("ok");
+}
+
+fn print_rainbow(text: &str) {
+    let mut generator = rand::thread_rng();
+    for letter in text.chars() {
+        print!(
+            "{}",
+            letter.to_string().truecolor(
+                generator.gen_range(100..=255),
+                generator.gen_range(100..=255),
+                generator.gen_range(100..=255)
+            )
+        )
+    }
+    println!();
 }
 
 fn load_database() -> Database {

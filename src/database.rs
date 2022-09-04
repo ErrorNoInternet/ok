785e49e5bdec3d0c1ed0edba3f2473cf6cb5f6a2
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-04 18:44:31 +0800

    Add database

--- src/database.rs
+++ src/database.rs
@@ -12,8 +12,45 @@ impl Database {
         }
     }
 
-    pub fn set(&self, key: &str, value: &str) -> Result<(), String> {
-        println!("{:?}", Path::new(self.path.as_str()).join(key));
-        Ok(())
+    pub fn set(&self, key: String, value: String) -> Result<(), String> {
+        if key.starts_with(".") {
+            return Err(String::from("key cannot begin with a dot!"));
+        }
+        match fs::write(Path::new(self.path.as_str()).join(key), value) {
+            Ok(_) => Ok(()),
+            Err(error) => Err(error.to_string()),
+        }
+    }
+
+    pub fn get(&self, key: String) -> Result<String, String> {
+        if key.starts_with(".") {
+            return Err(String::from("key cannot begin with a dot!"));
+        }
+        match fs::read(Path::new(self.path.as_str()).join(key)) {
+            Ok(value) => Ok(std::str::from_utf8(&value).unwrap().to_string()),
+            Err(error) => Err(error.to_string()),
+        }
+    }
+
+    pub fn delete(&self, key: String) -> Result<(), String> {
+        if key.starts_with(".") {
+            return Err(String::from("key cannot begin with a dot!"));
+        }
+        match fs::remove_file(Path::new(self.path.as_str()).join(key)) {
+            Ok(_) => Ok(()),
+            Err(error) => Err(error.to_string()),
+        }
+    }
+
+    pub fn keys(&self) -> Result<Vec<String>, String> {
+        let iterator = match fs::read_dir(&self.path) {
+            Ok(iterator) => iterator,
+            Err(error) => return Err(error.to_string()),
+        };
+        let mut keys = Vec::new();
+        for key in iterator {
+            keys.push(key.unwrap().file_name().into_string().unwrap());
+        }
+        Ok(keys)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 mod database;
+use database::Database;
 
-fn main() {}
+fn main() {
+    let mut database_path = String::from(".ok");
+    if cfg!(windows) {
+        database_path = format!("C:\\Users\\{}\\AppData\\Roaming\\ok", whoami::username())
+    } else if cfg!(unix) {
+        database_path = format!("/home/{}/.config/ok", whoami::username())
+    }
+    let db = match Database::open(database_path) {
+        Ok(db) => db,
+        Err(error) => {
+            println!("Unable to open database: {}", error);
+            return;
+        }
+    };
+}

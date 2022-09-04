e2ce1c37ca4d0febdfa60e0d1376f86556553706
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-04 20:58:20 +0800

    Add commands

---
+++ src/commands.rs
@@ -0,0 +1,9 @@
+pub fn statistics_command() {
+    println!("not implemented");
+}
+pub fn leaderboard_join_command() {
+    println!("not implemented");
+}
+pub fn leaderboard_leave_command() {
+    println!("not implemented");
+}

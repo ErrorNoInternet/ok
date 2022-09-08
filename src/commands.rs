b102e954ab1bcddd2bb31bb2c862e3572f8a91f2
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-08 22:02:34 +0800

    Switch read_line to read_char

--- src/commands.rs
+++ src/commands.rs
@@ -1,48 +1,50 @@
 use crate::database::Database;
 use chrono::{Datelike, TimeZone};
 use colored::Colorize;
+use console::Term;
 use std::{io::Write, ops::Index};
 use textplots::{Chart, Plot, Shape};
 
 pub fn reset_command(db: &Database) {
+    let terminal = Term::stdout();
     print!(
         "{} {} ",
         "Are you sure you want to reset your OK statistics?",
         "Y/N:".bold()
     );
     std::io::stdout().flush().unwrap();
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    if input.len() >= 1 {
-        let letter = input.chars().nth(0).unwrap().to_lowercase().to_string();
-        if letter == String::from("y") {
-            print!(
-                "{} {} ",
-                "Are you very sure you want to reset your OK statistics?".red(),
-                "Y/N:".bold().red()
-            );
-            std::io::stdout().flush().unwrap();
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-            if input.len() >= 1 {
-                let letter = input.chars().nth(0).unwrap().to_lowercase().to_string();
-                if letter == String::from("y") {
-                    let keys = match db.keys() {
-                        Ok(keys) => keys,
-                        Err(error) => {
-                            println!("Uh oh! There was an error: {}", error);
-                            return;
-                        }
-                    };
-                    for key in keys {
-                        match db.delete(key.clone()) {
-                            Ok(_) => (),
-                            Err(error) => println!("Unable to delete key ({}): {}", key, error),
-                        }
-                    }
-                    println!("{}", "Your OK statistics have been reset!".bold());
+    let letter = match terminal.read_char() {
+        Ok(letter) => letter,
+        Err(_) => 'n',
+    };
+    println!("{}", letter);
+    if letter == 'y' {
+        print!(
+            "{} {} ",
+            "Are you very sure you want to reset your OK statistics?".red(),
+            "Y/N:".bold().red()
+        );
+        std::io::stdout().flush().unwrap();
+        let letter = match terminal.read_char() {
+            Ok(letter) => letter,
+            Err(_) => 'n',
+        };
+        println!("{}", letter);
+        if letter == 'y' {
+            let keys = match db.keys() {
+                Ok(keys) => keys,
+                Err(error) => {
+                    println!("Uh oh! There was an error: {}", error);
+                    return;
+                }
+            };
+            for key in keys {
+                match db.delete(key.clone()) {
+                    Ok(_) => (),
+                    Err(error) => println!("Unable to delete key ({}): {}", key, error),
                 }
             }
+            println!("{}", "Your OK statistics have been reset!".bold());
         }
     }
 }
